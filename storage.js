b7fb2e520f19bd42776e9b649f120d55e3890d10
vetven964 <vet.ven2023@gmail.com>
2026-08-18 09:22:46 +0700

    fix: export storage event persistence for member registration

--- storage.js
+++ storage.js
@@ -112,7 +112,6 @@ async function cleanup() {
   }
 }
 
-
 async function loadAuthCredentials() {
   if (mode === 'postgres' && pool) {
     try {
@@ -148,4 +147,4 @@ async function saveAuthCredential(userId, passwordHash) {
   return {persistent:true,mode:'local'};
 }
 
-module.exports = { initStorage, saveQuote, saveAnalysis, getStatus, getHistory, cleanup, loadAuthCredentials, saveAuthCredential };
+module.exports = { initStorage, saveEvent, saveQuote, saveAnalysis, getStatus, getHistory, cleanup, loadAuthCredentials, saveAuthCredential };
